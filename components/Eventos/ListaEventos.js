f34320062e2d593277ad31fca4badbf2e84ea084
Matías Panza <panzamatiasezequiel@gmail.com>
2020-01-06 21:03:56 -0300

    puntos de interes con datos reales

--- components/Eventos/ListaEventos.js
+++ components/Eventos/ListaEventos.js
@@ -4,24 +4,40 @@ import { Ionicons } from '@expo/vector-icons';
 
 import ItemListaEventos from './ItemListaEventos'
 import HeaderComponent from '../Misc/HeaderComponent';
+import GetDatosAPI from '../Misc/GetDatosAPI';
 
-const evento1 = {
-    title: "Puntos Verdes",
-    texto: "HELLO",
-    fecha: "Durante todo el año",
-    img: "https://www.buenosaires.gob.ar/sites/gcaba/files/varela_gcba_apm__30.jpg"
-  };
-
-  const evento2 = {
-    title: "EcoBici",
-    texto: "HELLO",
-    fecha: "Durante todo el año",
-    img: "https://www.buenosaires.gob.ar/sites/gcaba/files/18.2.19_nueva_ecobici-0873.jpg"
+//hardcodeados por ahora
+const eventos = [{
+    title: "Centro de Reciclaje de la Ciudad",
+    texto: "Hello World",
+    fecha: "Lunes a Sábado",
+    img: "https://www.buenosaires.gob.ar/sites/gcaba/files/varela_gcba_apm__30.jpg",
   }
+];
+
+let eventoEcoBici = {
+  title: "EcoBici",
+    texto: "Hello World",
+    fecha: "Durante todo el año",
+    img: "https://www.buenosaires.gob.ar/sites/gcaba/files/18.2.19_nueva_ecobici-0873.jpg",
+    markers: []
+}
+
+let eventoPuntosVerdes = {
+  title: "Puntos Verdes",
+    texto: "Hello World",
+    fecha: "Durante todo el año",
+    img: "https://www.buenosaires.gob.ar/sites/gcaba/files/styles/interna_noticia/public/field/image/plaza_monsenor_de_andrea_2_recoleta_comuna_2.jpg",
+    markers: []
+}
 
 class ListaEventos extends React.Component {
   constructor (props) {
     super(props);
+    this.state={
+      eb: false,
+      pv: false
+    }
   }
 
   static navigationOptions = {
@@ -32,15 +48,54 @@ class ListaEventos extends React.Component {
     ),
   };
 
+  componentDidMount(){
+    GetDatosAPI.getDatosEcoBici(this.handleMarkers.bind(this));
+    GetDatosAPI.getDatosPuntosVerdes(this.handlePuntosVerdes.bind(this));
+  }
+  
+
+  renderEcoBici(navigation){
+    if(this.state.eb){
+      return(
+        <ItemListaEventos evento={eventoEcoBici} backgroundImage={eventoEcoBici.img} navigation={navigation}/>
+      )
+    }else
+      return null;
+  }
+
+  renderPuntosVerdes(navigation){
+    if(this.state.pv){
+      return(
+        <ItemListaEventos evento={eventoPuntosVerdes} backgroundImage={eventoPuntosVerdes.img} navigation={navigation}/>
+      )
+    }else
+      return null;
+  }
+
+  //hardcodeado por ahora
+  handleMarkers(markers){
+    eventoEcoBici.markers = markers;
+    this.setState({eb: true})
+  }
+
+  handlePuntosVerdes(markers){
+    eventoPuntosVerdes.markers = markers;
+    this.setState({pv: true})
+  }
+  
   
   render() {
     const { navigation } = this.props;
-
     return(
       <View style={{flex:1}}>
         <HeaderComponent tittle={"Puntos de interés"}/>
-        <ItemListaEventos key={1} evento={evento1} backgroundImage={'https://www.buenosaires.gob.ar/sites/gcaba/files/varela_gcba_apm__30.jpg'} navigation={navigation}/>
-        <ItemListaEventos key={2} evento={evento2} backgroundImage={'https://www.buenosaires.gob.ar/sites/gcaba/files/18.2.19_nueva_ecobici-0873.jpg'} navigation={navigation}/>
+        {eventos.map((evento, i) => {
+          return(
+            <ItemListaEventos key={i} evento={evento} backgroundImage={evento.img} navigation={navigation}/>
+          )
+        })}
+        {this.renderPuntosVerdes(navigation)}
+        {this.renderEcoBici(navigation)}
       </View>
     );
   }
---
+++ components/Misc/GetDatosAPI.js
@@ -0,0 +1,50 @@
+
+export default new class GetDatosAPI {
+
+    createDataEB(station){
+        return({
+            lat: station.lat,
+            long: station.lon,
+            title: station.name,
+            desc: station.address
+        })
+    }
+
+    createDataPV(obj){
+        return({
+            lat: obj.lat,
+            long: obj.long,
+            title: obj.plaza,
+            desc: obj.dias_y_horario
+        })
+    }
+
+    getDatosEcoBici(handleData){
+        const urlEcobici = "https://apitransporte.buenosaires.gob.ar/ecobici/gbfs/stationInformation?client_id=482c5a921298430f94982536c55b6b9b&client_secret=0d831E412e0C4eF0B20598477B87BCCf";
+
+        fetch(urlEcobici).then((response) => {
+            return response.json();
+        }).then((responseData) => {
+            const stations = responseData.data.stations;
+
+            let markers = [];
+            for(let i = 0; i<stations.length; i++){
+                markers.push(this.createDataEB(stations[i]));
+            }
+            console.log("LISTO REY")
+            handleData(markers)
+        })
+    }
+
+    getDatosPuntosVerdes(handleData){
+        var json = require("../../resources/puntos_verdes.json");
+        
+        let markers = [];
+        for(let i = 0; i < json.length; i++){
+            markers.push(this.createDataPV(json[i]));
+        }
+        console.log("LISTO KING");
+        handleData(markers)
+    }
+
+}
